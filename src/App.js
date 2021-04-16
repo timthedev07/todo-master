327c568fd2c71963a07a1a6a2a4b051f9bc27ba1
Harvard90873 <65774333+Harvard90873@users.noreply.github.com>
2021-04-16 19:00:11 +0200

    initial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import { Heading } from "./components/Heading";
+import './css/styles.css';
+
+class App extends Component{
+    render() {
+        return (
+            <Heading />
+        );
+    }
+  
+}
+
+export default App;
---
+++ src/components/Heading.js
@@ -0,0 +1,9 @@
+import { Component } from "react";
+
+export class Heading extends Component {
+    render() {
+        return (
+            <h1 id="page-heading">Todo Master</h1>
+        )
+    }
+}
